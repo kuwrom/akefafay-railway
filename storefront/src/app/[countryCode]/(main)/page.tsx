f01ecc47772ab0f5b854eae131728c1fce7a3eb7
kuwrom <kidanekibrom@gmail.com>
2025-02-22 19:44:47 +0300

    initial mod

--- storefront/src/app/[countryCode]/(main)/page.tsx
+++ storefront/src/app/[countryCode]/(main)/page.tsx
@@ -6,9 +6,9 @@ import { getCollectionsWithProducts } from "@lib/data/collections"
 import { getRegion } from "@lib/data/regions"
 
 export const metadata: Metadata = {
-  title: "Medusa Next.js Starter Template",
+  title: "Akefafay",
   description:
-    "A performant frontend ecommerce starter template with Next.js 14 and Medusa.",
+    "Ethiopian best wholesale marketplace.",
 }
 
 export default async function Home({
